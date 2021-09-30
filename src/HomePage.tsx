1f78dea7c451a9979e3f3ec35a813e3e1317f7fc
Ittapopo <stianstian95gmail.com>
2021-09-30 15:18:10 +0200

    Adding HomePage component(minor fix)

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const HomePage = () => (
     <div>
         <div>
-            <h2>New Meeting</h2>
+            <h2>Unanswered Meeting</h2>
             <button>Join meeting</button>
         </div>
     </div>
